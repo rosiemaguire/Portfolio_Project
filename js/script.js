33db6c07590579a0345a71c82a7788f3d23c6e0c
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-10-29 15:28:36 +1000

    adding slick

--- js/script.js
+++ js/script.js
@@ -15,3 +15,14 @@ exitButton.addEventListener("click",() => {
   exitButton.classList.toggle("display-none")
   hamburgerButton.classList.toggle("display-none")
 })
+
+document.addEventListener("DOMContentLoaded", function(){
+  $('.project-wrapper').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    arrows: true
+    // prevArrow:"<img class='a-left control-c prev slick-prev' src='images/icons8-previous-50.png'>",
+    // nextArrow:"<img class='a-right control-c next slick-next' src='images/icons8-next-50.png'>"
+  });
+});
